488fd7ac023011497b6eaadcda2ebdc0b9f296e0
1496-rajeev <1496piku@gmail.com>
2020-12-07 07:32:00 +0530

    7 December

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import './Home/index';
+import React from 'react';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <Home />
+//     </div>
+//   );
+// }
+
+
+
+// function Welcome(props) {
+// return <h1>hello, {props.name}</h1>
+// }
+
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+function App(){
+  const numbers = [1, 2, 3, 4, 5];
+  const doubled = numbers.map((number) => number * 2);
+  console.log(doubled);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+         <Welcome name="sar"/>
     </div>
-  );
+  )
 }
 
 export default App;
